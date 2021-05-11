9373d23eb3dbc5f0260cd47bda8cbe83a36afe11
aadeshdubey <aadesh.dubey@yahoo.com>
2021-05-12 01:03:20 +0530

    updating profile information

--- spinup-ec2.tf
+++ spinup-ec2.tf
@@ -10,14 +10,15 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
+  profile = "opsuser"
   region  = "ap-south-1"
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-010aff33ed5991201"
   instance_type = "t2.micro"
-
+  key_name      = "ec2"
+  vpc_security_group_ids=["security-group-ec2"]
   tags = {
     Name = var.instance_name
   }
